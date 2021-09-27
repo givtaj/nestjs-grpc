cd5fffb254268b85f1940704fb4f033f291be802
Aidin Givtaj <givtaj@gmail.com>
2021-09-27 23:03:00 +0400

    added testing flag for server url

--- nest-grpc-client/src/grpc.options.ts
+++ nest-grpc-client/src/grpc.options.ts
@@ -1,11 +1,14 @@
 import { ClientOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 
+const grpcServerAddress =
+  process.env.NODE_ENV === 'test' ? '0.0.0.0' : 'msserver';
+
 export const microserviceOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     // need to add url for docker compose
-    url: 'msserver:9090',
+    url: `${grpcServerAddress}:9090`,
     package: 'app',
     protoPath: join(__dirname, '../src/app.proto'),
   },
